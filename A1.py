5645a157f657a9917d2d76792aa7731bcc473aaa
RKXKEAN <roykaen.30@gmail.com>
2024-09-21 12:05:59 +0700

    last Modify

--- A1.py
+++ A1.py
@@ -1,1 +1,14 @@
-print("HI I NHA NGO I NHA Nung tad")
+scores_dict = {}
+for person in range(1, 6):
+    scores = input().split()
+    total = 0
+    for score in scores:
+        total += int(score)
+    scores_dict[person] = total
+
+print(scores_dict)
+
+maximum_score = max(scores_dict.values())
+for key, value in scores_dict.items():
+    if value == maximum_score:
+        print(key, value)
